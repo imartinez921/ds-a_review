385118e2ecbee13445e470146ef5ab0f354fbafc
Irene D. Martinez <imartinez921@gmail.com>
2024-01-09 19:09:04 -0500

    Practice writing own unit tests for DS&A problems

--- generateDocument.js
+++ generateDocument.js
@@ -28,55 +28,76 @@ function generateDocument(characters, document) {
 	return true;
 }
 
-// Solution 2 using a count Hash to compare letter quantities
-function generateDocument(characters, document) { // (car, care)
-	// create count hash for char string
-	const countChars = {}; // {A:1}
-	for (let char of characters) {// r
-		// if (countChars[char] === undefined) {
-		// 	countChars[char] = 1;
-		// } else {
-		// 	countChars[char] += 1;
-		// }
-        countChars[char] = ++countChars[char] || 1
-	}
+// valid case
+let characters = "abcdefg"
+let document = "abcdef"
+console.log(generateDocument(characters,document))
+
+// not enough characters
+characters = "abc"
+document = "abcdef"
+console.log(generateDocument(characters,document) == false)
+
+// integers
+try {
+	console.log(generateDocument(true, document));
+	console.log(false)
+} catch(error){
+	console.log(true)
+}
+
+// empty characters
+// empty document
+
+// // Solution 2 using a count Hash to compare letter quantities
+// function generateDocument(characters, document) { // (car, care)
+// 	// create count hash for char string
+// 	const countChars = {}; // {A:1}
+// 	for (let char of characters) {// r
+// 		// if (countChars[char] === undefined) {
+// 		// 	countChars[char] = 1;
+// 		// } else {
+// 		// 	countChars[char] += 1;
+// 		// }
+//         countChars[char] = ++countChars[char] || 1
+// 	}
     
-	// iterate through doc
-	for (let char of document) { // e
-		// if char in doc is in char count hash
-		if (countChars[char] > 0) {
-			// key in and decrement value
-			countChars[char] -= 1;
-		} else {
-			// if char in doc is NOT in char count hash
-			// return false
-			return false;
-		}
-	}
-	return true;
-}
+// 	// iterate through doc
+// 	for (let char of document) { // e
+// 		// if char in doc is in char count hash
+// 		if (countChars[char] > 0) {
+// 			// key in and decrement value
+// 			countChars[char] -= 1;
+// 		} else {
+// 			// if char in doc is NOT in char count hash
+// 			// return false
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
 
 
 
-// Solution 3 using recursion
-// t complexity = .includes conditional is O(m) and has .slice O(n)
-    // O(n) to traverse entire characters string
-    // * O(m), bc we each time traverse document in .includes
-    // * O(n), bc we also 
-function generateDocument(characters, document) {
-	// base case
-	// if characters.length === 0, return whether document has any letters left
-	if (characters.length === 0) return document.length === 0;
-	// take the last letter of characters
-	let checkChar = characters[characters.length - 1];
-	// check if document has that char
-	if (document.includes(checkChar)) {
-		// if yes, splice it out of document
-		let index = document.indexOf(checkChar);
-		document = document.slice(0, index) + document.slice(index + 1);
-	}
-	// take off the last letter of characters
-	characters = characters.slice(0, characters.length - 1);
-	// make recursive call using new characaters and remaining document string
-	return generateDocument(characters, document);
-}
+// // Solution 3 using recursion
+// // t complexity = .includes conditional is O(m) and has .slice O(n)
+//     // O(n) to traverse entire characters string
+//     // * O(m), bc we each time traverse document in .includes
+//     // * O(n), bc we also 
+// function generateDocument(characters, document) {
+// 	// base case
+// 	// if characters.length === 0, return whether document has any letters left
+// 	if (characters.length === 0) return document.length === 0;
+// 	// take the last letter of characters
+// 	let checkChar = characters[characters.length - 1];
+// 	// check if document has that char
+// 	if (document.includes(checkChar)) {
+// 		// if yes, splice it out of document
+// 		let index = document.indexOf(checkChar);
+// 		document = document.slice(0, index) + document.slice(index + 1);
+// 	}
+// 	// take off the last letter of characters
+// 	characters = characters.slice(0, characters.length - 1);
+// 	// make recursive call using new characaters and remaining document string
+// 	return generateDocument(characters, document);
+// }
